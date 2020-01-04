48ab74b16d111445f6ee7f24b1575a091b12c551
mvetere1 <58411412+mvetere1@users.noreply.github.com>
2020-01-03 23:32:33 -0500

    Update FrequencyCounter.ino

--- FrequencyCounter/FrequencyCounter.ino
+++ FrequencyCounter/FrequencyCounter.ino
@@ -1,12 +1,15 @@
 int pin = 7;
-unsigned long duration;
-
-void setup() {
+unsigned long durationUs;
+float frequency = 0.0f;
+  
+void setup() 
+{
   Serial.begin(9600);
   pinMode(pin, INPUT);
 }
 
 void loop() {
-  duration = pulseIn(pin, HIGH);
-  Serial.println(duration);
+  durationUs = pulseIn(pin, HIGH);
+  frequency = 1000000/durationUs;
+  Serial.println(frequency);
 }
