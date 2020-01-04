2b03a9a1a521141944445e38877a6be2d3468be9
mvetere1 <58411412+mvetere1@users.noreply.github.com>
2020-01-04 00:52:02 -0500

    Update FrequencyCounter.ino

--- FrequencyCounter/FrequencyCounter.ino
+++ FrequencyCounter/FrequencyCounter.ino
@@ -1,26 +1,30 @@
-int pin = 7;
+
 unsigned long durationUs = 0;
 float frequency = 0.0f;
 float dutyCycle = 0;
-const int cycleCount = 250;
-const int xMin = 241;
-const int xMax = 348;
-const float slope = (100)/(348-244);
+const int cycleCount = 100;
+const int xMin = 247;
+const int xMax = 352;
+const float slope = (100)/float((xMax-xMin));
 
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(pin, INPUT);
+  pinMode(7, INPUT);
+  pinMode(11,OUTPUT);
 }
 
 void loop() {
   for (int i = 0; i < cycleCount; i++)
   {
-     durationUs = pulseIn(pin, HIGH);
+     durationUs = pulseIn(7, HIGH);
      frequency += (1000000/durationUs)/2;
   }
   frequency = frequency/cycleCount;
-  dutyCycle = (frequency - xMin)*.96;
+  dutyCycle = (frequency - xMin)*slope;
+  
+  analogWrite(11,int(dutyCycle*2.55));
+  
   Serial.println(frequency);
   Serial.println(dutyCycle);
 }
