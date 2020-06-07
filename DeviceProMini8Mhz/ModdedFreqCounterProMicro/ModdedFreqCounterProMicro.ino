980c6f345e2eb8948cbb8441bd2132c97888278c
mvetere1 <58411412+mvetere1@users.noreply.github.com>
2020-06-07 01:06:28 -0400

    refactoring

--- DeviceProMini8Mhz/ModdedFreqCounterProMicro/ModdedFreqCounterProMicro.ino
+++ DeviceProMini8Mhz/ModdedFreqCounterProMicro/ModdedFreqCounterProMicro.ino
@@ -1,10 +1,6 @@
-
-unsigned long durationUs = 0;
-float frequency = 0.0f;
 float dutyCycle = 0;
-const int cycleCount = 100;
-const int xMin = 190;
-const int xMax = 300;
+volatile int xMin = 190;
+volatile int xMax = 300;
 const float slope = (100)/float((xMax-xMin));
 
 //new stuff, adding pot voltage reading
@@ -16,17 +12,58 @@ void setup()
   pinMode(11,OUTPUT);
   //TCCR2B = TCCR2B & B11111000 | B00000110; //122 hz PWM
   TCCR2B = TCCR2B & B11111000 | B00000111;  //30 hz pwm
+  attachInterrupt(digitalPinToInterrupt(2),updateXMin,RISING);
+  attachInterrupt(digitalPinToInterrupt(3),updateXMax,RISING);
+  
 }
 
 void loop() {
+  float frequency = getFrequency();
+  dutyCycle = freqToDutyCycle(frequency);
+  dutyCycle = clampDutyCycle(dutyCycle);
+
+  analogWrite(11,int(dutyCycle*2.55));
+  
+  Serial.println(frequency);
+  Serial.println(dutyCycle);
+}
+void updateXMin()
+{
+  xMin = 99;
+  //need to get current freq when block is black
+}
+
+void updateXMax()
+{
+  xMax = 193;
+  //need to get current freq when block is red
+}
+
+float freqToDutyCycle(float freq)
+{
+
+  dutyCycle = (freq - xMin)*slope;
+
+  return dutyCycle;
+}
+
+float getFrequency()
+{
+  float frequency = 0.0f;
+  unsigned long durationUs = 0;
+  const int cycleCount = 100;
+  
   for (int i = 0; i < cycleCount; i++)
   {
      durationUs = pulseIn(7, HIGH);
      frequency += (1000000/durationUs)/2;
   }
   frequency = frequency/cycleCount;
-  dutyCycle = (frequency - xMin)*slope;
+  return frequency;
+}
 
+float clampDutyCycle(float dutyCycle)
+{
   if(dutyCycle > 100)
   {
     dutyCycle = 100;
@@ -35,9 +72,8 @@ void loop() {
   {
     dutyCycle = 0;
   }
-  analogWrite(11,int(dutyCycle*2.55));
   
-  Serial.println(frequency);
-  Serial.println(dutyCycle);
+  return dutyCycle;
+
 }
 //
