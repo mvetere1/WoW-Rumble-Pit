43593d0758fb99701204eb945cf9126b213f37f6
mvetere1 <58411412+mvetere1@users.noreply.github.com>
2020-07-01 23:36:41 -0400

    did pcb layout and added some status stuff to code

--- DeviceProMini8Mhz/ModdedFreqCounterProMicro/ModdedFreqCounterProMicro.ino
+++ DeviceProMini8Mhz/ModdedFreqCounterProMicro/ModdedFreqCounterProMicro.ino
@@ -1,13 +1,29 @@
 volatile int xMin = 0;
 volatile int xMax = 0;
 
-  float dutyCycle = 0;
+//float dutyCycle = 0;
 
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(7, INPUT); //will prob need to switch to 4 for the micro pro circuit
-  pinMode(11,OUTPUT);
+  //Connections with other modules
+  pinMode(10,INPUT); //Frequency read in from color sensor
+  pinMode(11,OUTPUT); //PWM output to motor driver
+  
+  //These output pinModes are for LED status stuff
+  pinMode(4,OUTPUT); // Power 100% Blue or orange
+  pinMode(5,OUTPUT); // Power 80% Green or purple
+  pinMode(6,OUTPUT); // Power 60% Green or blue
+  pinMode(7,OUTPUT); // Power 40% Yellow or green
+  pinMode(8,OUTPUT); // Power 20% Yellow or white
+  pinMode(9,OUTPUT); // Power  0% Red or red
+
+  //Indicator LEDs
+  pinMode(12,OUTPUT); // Black calibration indicator (0% red of 300-400 hz)
+  pinMode(13,OUTPUT); // Red calibration indicator (100% red or 600-800 hz)
+  pinMode(14,OUTPUT); // Calibrated and working
+  pinMode(15,OUTPUT); // anything?
+  
   //TCCR2B = TCCR2B & B11111000 | B00000110; //122 hz PWM
   TCCR2B = TCCR2B & B11111000 | B00000111;  //30 hz pwm
   attachInterrupt(digitalPinToInterrupt(2),updateXMin,RISING);
@@ -15,37 +31,52 @@ void setup()
   
 }
 
-void loop() {
-  //xMin and xMax are only set via button press interrupts
-  if (xMin && xMax){
-      float frequency = getFrequency();
+void loop() 
+{
+//xMin and xMax are only set via button press interrupts
+float frequency = 0;
+float dutyCycle = 0;
+float processedDutyCycle = 0;
+while(1)
+  {
+  if (xMin && xMax)
+    {
+    digitalWrite(14,HIGH);
+    while(1)
+      {
+      frequency = getFrequency();
       dutyCycle = freqToDutyCycle(frequency);
-      dutyCycle = processDutyCycle(dutyCycle);
-    
-      analogWrite(11,int(dutyCycle));
+      
+      processedDutyCycle = processDutyCycle(dutyCycle);
+      updateStatusLEDs(processedDutyCycle);
+      analogWrite(11,int(processedDutyCycle));
       
       Serial.println(frequency);
-      Serial.println(dutyCycle);
+      Serial.println(processedDutyCycle);
+      }
+    }
   }
 }
+//need to get current freq when block is black
 void updateXMin()
 {
   xMin = getFrequency();
-  //need to get current freq when block is black
+  digitalWrite(12,HIGH);
+  
 }
-
+//need to get current freq when block is red
 void updateXMax()
 {
   xMax = getFrequency();
-  //need to get current freq when block is red
+  digitalWrite(13,HIGH);
+  
 }
 
 float freqToDutyCycle(float freq)
 {
   float slope = (100)/float((xMax-xMin));
-  dutyCycle = (freq - xMin)*slope;
+  return (freq - xMin)*slope;
 
-  return dutyCycle;
 }
 
 float getFrequency()
@@ -76,5 +107,39 @@ float processDutyCycle(float dutyCycle)
   
   return dutyCycle*2.55;
 
+}
+void updateStatusLEDs(float dutyCycle)
+{
+  digitalWrite(9,HIGH);
+  
+  if (dutyCycle > 20){
+      digitalWrite(8,HIGH);
+  }
+  else
+      digitalWrite(8,LOW);
+      
+  if (dutyCycle> 40){
+      digitalWrite(7,HIGH);
+  }
+  else
+      digitalWrite(7,LOW);
+      
+  if (dutyCycle > 60){
+      digitalWrite(6,HIGH);
+  }
+  else
+      digitalWrite(6, LOW);
+      
+  if (dutyCycle > 80){
+      digitalWrite(5,HIGH);
+  }
+  else
+      digitalWrite(5,LOW);
+  
+  if (dutyCycle > 100) {
+      digitalWrite(4,HIGH);
+  }
+  else
+      digitalWrite(4,LOW);
 }
 //
